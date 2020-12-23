6bcfd53fc89fb499d2b5a6fcef8dbd8cd25a5509
Lavisha Bhambri <bhambrilavisha@gmail.com>
2020-12-23 22:37:09 +0530

    Create Array Intersection.cpp

---
+++ Lecture 5 : Time and Space Complexity Analysis/Array Intersection.cpp
@@ -0,0 +1,145 @@
+/*
+You have been given two integer arrays/list(ARR1 and ARR2) of size N and M, respectively. You need to print their intersection; An intersection for this problem can be defined when both the arrays/lists contain a particular value or to put it in other words, when there is a common value that exists in both the arrays/lists.
+Note :
+Input arrays/lists can contain duplicate elements.
+
+The intersection elements printed would be in the order they appear in the first sorted array/list(ARR1).
+
+
+Input format :
+The first line contains an Integer 't' which denotes the number of test cases or queries to be run. Then the test cases follow.
+
+The first line of each test case or query contains an integer 'N' representing the size of the first array/list.
+
+The second line contains 'N' single space separated integers representing the elements of the first the array/list.
+
+The third line contains an integer 'M' representing the size of the second array/list.
+
+The fourth line contains 'M' single space separated integers representing the elements of the second array/list.
+Output format :
+For each test case, print the intersection elements in a row, separated by a single space.
+
+Output for every test case will be printed in a separate line.
+Constraints :
+1 <= t <= 10^2
+0 <= N <= 10^6
+0 <= M <= 10^6
+
+Time Limit: 1 sec 
+Sample Input 1 :
+2
+6
+2 6 8 5 4 3
+4
+2 3 4 7 
+2
+10 10
+1
+10
+Sample Output 1 :
+2 3 4
+10
+Sample Input 2 :
+1
+4
+2 6 1 2
+5
+1 2 3 4 2
+Sample Output 2 :
+1 2 2
+Explanation for Sample Output 2 :
+Since, both input arrays have two '2's, the intersection of the arrays also have two '2's. The first '2' of first array matches with the first '2' of the second array. Similarly, the second '2' of the first array matches with the second '2' if the second array.
+*/
+
+void merge(int arr[], int l, int m, int r)
+{
+    int n1 = m - l + 1;
+    int n2 = r - m;
+ 
+    // Create temp arrays
+    int L[n1], R[n2];
+ 
+    // Copy data to temp arrays L[] and R[]
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+ 
+    // Merge the temp arrays back into arr[l..r]
+ 
+    // Initial index of first subarray
+    int i = 0;
+ 
+    // Initial index of second subarray
+    int j = 0;
+ 
+    // Initial index of merged subarray
+    int k = l;
+ 
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+ 
+    // Copy the remaining elements of
+    // L[], if there are any
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+ 
+    // Copy the remaining elements of
+    // R[], if there are any
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+ 
+// l is for left index and r is
+// right index of the sub-array
+// of arr to be sorted */
+void mergeSort(int arr[],int l,int r){
+    if(l>=r){
+        return;//returns recursively
+    }
+    int m = (l+r-1)/2;
+    mergeSort(arr,l,m);
+    mergeSort(arr,m+1,r);
+    merge(arr,l,m,r);
+}
+ 
+//Finding intersection and printing only when both the elements are equal.
+void intersection(int *arr1, int *arr2, int n, int m) 
+{
+    //Write your code here
+    mergeSort(arr1,0,n-1);
+    mergeSort(arr2,0,m-1);
+    
+    
+    int i=0,j=0;
+    while(i<n && j<m){
+        if(arr1[i] ==arr2[j]){
+            cout<<arr1[i]<<" ";
+            //arr2[j]=-10000;
+            i++;
+            j++;
+        }
+        else if(arr1[i]<arr2[j]){
+            i++;
+        }
+        else{
+            j++;
+        }
+    }
+    
+}
